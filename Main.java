1017ef26e4b11a523e5fff66fce1a5e924e3c47b
Gudykson <127319413+Gudykson@users.noreply.github.com>
2023-03-22 13:36:25 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+    string str = "Hello world";
+    for (int i = 0; i < 3; i++) {
+      for (int j = 0; j < str.lenght(); j++) {
+        System.out.println(str.charAt(j));
+      }
+      System.out.println("");
+    }
+  }
+}
